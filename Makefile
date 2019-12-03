916c593c2859e9c547cc7492b4c271b3d06ae034
Jon Robison <narfman0@gmail.com>
2019-12-03 11:46:36 -0500

    -Update to new deploy process and new best practices (since 3 yrs ago)

--- Makefile
+++ Makefile
@@ -47,6 +47,9 @@ clean-test: ## remove test and coverage artifacts
 	rm -f .coverage
 	rm -fr htmlcov/
 
+build:
+	python setup.py sdist bdist_wheel
+
 lint: ## check style with flake8
 	flake8 twitter_timeline_rss tests
 
@@ -62,14 +65,8 @@ coverage: ## check code coverage quickly with the default Python
 	coverage html
 	$(BROWSER) htmlcov/index.html
 
-release: clean ## package and upload a release
-	python setup.py sdist upload
-	python setup.py bdist_wheel upload
-
-dist: clean ## builds source and wheel package
-	python setup.py sdist
-	python setup.py bdist_wheel
-	ls -l dist
+release: clean build ## package and upload a release
+	twine upload --repository pypi dist/*
 
 install: clean ## install the package to the active Python's site-packages
 	python setup.py install
--- twitter_timeline_rss/cli.py
+++ twitter_timeline_rss/cli.py
@@ -4,9 +4,9 @@ import argparse
 from twitter_timeline_rss.twitter import feed, home
 
 
-parser = argparse.ArgumentParser(description='Dump twitter RSS feeds')
-parser.add_argument('--user', help='User from which to ingest timeline')
-parser.add_argument('--file', help='Dump to specified file instead of stdout')
+parser = argparse.ArgumentParser(description="Dump twitter RSS feeds")
+parser.add_argument("--user", help="User from which to ingest timeline")
+parser.add_argument("--file", help="Dump to specified file instead of stdout")
 args = parser.parse_args()
 
 
@@ -14,9 +14,13 @@ def main():
     response_object = feed(args.user) if args.user else home()
     response_text = response_object.response
     if isinstance(response_text, list):
-        response_text = b''.join(response_text)
+        response_text = b"".join(response_text)
     if args.file:
-        with open(args.file, 'w') as f:
+        with open(args.file, "wb") as f:
             f.write(response_text)
     else:
         print(response_text)
+
+
+if __name__ == "__main__":
+    main()
